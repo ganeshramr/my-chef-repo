9d36da6ad6bd3d01a138189d36a47862fdfcdba6
root <root@precise64.(none)>
2015-06-17 19:32:27 +0000

    initial version

---
+++ cookbooks/liferay/attributes/default.rb
@@ -0,0 +1,6 @@
+default.liferay.crossContext="true"
+default.java.jdk_version=7
+default.java.oracle.accept_oracle_download_terms=true
+default.java.install_flavor="oracle"
+default.tomcat.catalina_options="$CATALINA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+
