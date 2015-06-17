ef982b9c90e62d70f812015c80bb5bf2407b1b3b
root <root@precise64.(none)>
2015-06-17 21:05:04 +0000

    cleanup

--- cookbooks/liferay/attributes/default.rb
+++ cookbooks/liferay/attributes/default.rb
@@ -3,4 +3,4 @@ default.java.jdk_version=7
 default.java.oracle.accept_oracle_download_terms=true
 default.java.install_flavor="oracle"
 default.tomcat.catalina_options="$CATALINA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
-
+default.tomcat.base_version=7
---
+++ cookbooks/liferay/metadata.rb
@@ -0,0 +1,10 @@
+name             'liferay'
+maintainer       'ganeshramr'
+maintainer_email 'mailganesh@icould.com'
+license          'All rights reserved'
+description      'Cookbook for liferay in-memory db version.Runs on precise64 3.2.0-23-generic Vagrant image'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '2.0.1'
+supports 'ubuntu'
+
+depends 'tomcat', '~> 0.17.3'
